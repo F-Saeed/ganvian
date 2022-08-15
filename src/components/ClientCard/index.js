8ff50a372af184760e67c83a555471902a9deb80
Faran Saeed <faransaeed@rocketmail.com>
2022-08-15 19:08:24 +0500

    working on section 6 carousel

---
+++ src/components/ClientCard/index.js
@@ -0,0 +1,7 @@
+import styles from './ClientCard.module.scss';
+
+const ClientCard = () => {
+  return <div className={styles.clientCard}>ClientCard</div>;
+};
+
+export default ClientCard;
--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -8,9 +8,17 @@ import productManagementLogo from '../../assets/images/product_management_logo.p
 import appleLogo from '../../assets/images/apple_logo.png';
 import androidLogo from '../../assets/images/android_logo.png';
 import computerLogo from '../../assets/images/computer_logo.png';
+import { ReactComponent as DecorationLayer2 } from '../../assets/images/decoration_layer_2.svg';
+import frame3 from '../../assets/images/frame_3.png';
+import { ReactComponent as DecorationLayer3 } from '../../assets/images/decoration_layer_3.svg';
+import frame4 from '../../assets/images/frame_4.png';
 
 // Styles
 import styles from './Hero.module.scss';
+import ClientCard from '../ClientCard';
+
+// External
+import { Carousel } from '@trendyol-js/react-carousel';
 
 const Hero = () => {
   return (
@@ -32,9 +40,11 @@ const Hero = () => {
         </div>
         <Frame1 />
       </section>
+
       <h1>
         Our <span>Company</span>
       </h1>
+
       <section className={styles.section2}>
         <img src={frame2} alt='Boy and Girl looking at tablet' />
         <div>
@@ -46,21 +56,69 @@ const Hero = () => {
             We always seek the best way to help our customers with good agility.
           </p>
           <button type='button' className='primary'>
-            Free Consultation
+            Our Company
           </button>
           <button type='button' className='secondary'>
-            Read More
+            Learn More
           </button>
         </div>
       </section>
+
       <h1>
         Our <span>Services</span>
       </h1>
+
       <section className={styles.section3}>
         <ServiceCard logo={productManagementLogo} title='Product Management' />
         <ServiceCard logo={appleLogo} title='iOS Development' />
         <ServiceCard logo={androidLogo} title='Android Development' />
         <ServiceCard logo={computerLogo} title='Web Development' />
+      </section>
+
+      <section className={styles.section4}>
+        <DecorationLayer2 />
+        <div>
+          <h1>
+            Our App <span>Development Services</span>
+          </h1>
+          <p>
+            We focus on delivering world-class quality and bespoke application
+            development solutions that expand your reach, improve your customer
+            engagement, and take your business to the next level.
+          </p>
+          <button type='button' className='primary'>
+            Our Services
+          </button>
+        </div>
+        <img src={frame3} alt='Boy working on computer' />
+      </section>
+
+      <section className={styles.section5}>
+        <DecorationLayer3 />
+        <img src={frame4} alt='People looking at tablet' />
+        <div>
+          <h1>
+            We Are Transforming the
+            <span>Digital Landscape</span>
+          </h1>
+          <p>
+            We take care of your business needs by bringing novel digital
+            experiences to the table by flawlessly integrating design &
+            development. Our team offers end-to-end full stack development
+            services blended with
+          </p>
+          <button type='button' className='primary'>
+            Free Consultation
+          </button>
+        </div>
+      </section>
+      <section className={styles.section6}>
+        <DecorationLayer2 />
+        <Carousel show={3.5} slide={3} swiping={true}>
+          <ClientCard />
+          <ClientCard />
+          <ClientCard />
+        </Carousel>
       </section>
     </>
   );
